2bbec5367b02e3d2df4127eee3259947a0402736
Vincent Rodley <vindognz@gmail.com>
2025-08-08 11:53:21 +1200

    im not going crazy.

---
+++ main.py
@@ -0,0 +1,12 @@
+print("This will become the game!!!!!")
+print("Wakatime please sync so i can start the thingy")
+print('please wakatime. let me make my project pleaseeee')
+print("HURRY UP AND SYNC SO I CAN MAKE MY SUMMER PROJECT PLEASE")
+print("i am going to crash tf out if it doesnt sync in the next 30 seconds")
+
+i = 0
+while i < 30:
+    print(i)
+
+print("times up. SYNC YOU STUPID BOZO")
+print("thank you")
